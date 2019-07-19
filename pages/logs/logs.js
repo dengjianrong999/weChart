8cb71645f1cf90e215fb192ef49b5138f8f3ef64
dengjianrong999 <15811865729@163.com>
2019-07-19 10:19:05 +0800

    page util

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,78 @@
-//logs.js
-const util = require('../../utils/util.js')
-
+// pages/mine/mine.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    logs: []
+    lisT: [
+      { src: 'collection', text: '最新电音' },
+      { src: 'customerservice', text: '私人FM' },
+      { src: 'tasklist', text: 'Sati空间' },
+      { src: 'message', text: '私藏推荐' },
+      { src: 'keyboard', text: '因乐交友' },
+      { src: 'integral', text: '古典专区' },
+      { src: 'flag', text: '跑步FM' },
+      { src: 'offline', text: '小冰电台' },
+      { src: 'remind', text: '爵士电台' },
+      { src: 'shop_fill', text: '驾驶模式' },
+    ]
+
   },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
